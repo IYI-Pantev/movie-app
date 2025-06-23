4695d3192dca9c94f7a751d5b2204f8643da7029
Nick Pantev <103816194+IYI-Pantev@users.noreply.github.com>
2025-06-23 23:04:30 +0300

    Movie Card created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,68 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Search from "./components/Search";
+import Spinner from "./components/Spinner";
+import MovieCard from "./components/MovieCard";
+
+const API_BASE_URL: string = "https://api.themoviedb.org/3";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 
 function App() {
   const [searchTerm, setSearchTerm] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  interface Movie {
+    id: number;
+    original_title: string;
+    vote_average: number;
+    poster_path: string;
+    release_date: string;
+    original_language: string;
+    // add other properties as needed
+  }
+
+  const [movieList, setMovieList] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage(null);
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies");
+      }
+
+      const data = await response.json();
+      if (data.Response === "False") {
+        setErrorMessage(data.Error || "Failed to fetch movies");
+        setMovieList([]);
+        return;
+      }
+      // console.log(data);
+
+      setMovieList(data.results || []);
+    } catch (error) {
+      console.log(`Error fetching movies: ${error}`);
+      setErrorMessage("Error fetching movies. Please try again later.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <main>
       <div className="pattern" />
@@ -13,8 +73,22 @@ function App() {
             Find <span className="text-gradient">Movies</span> You'll Enjoy
             Without the Hassle
           </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <section className="all-movies">
+          <h2 className="mt-2">All Movies</h2>
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-600">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )}
+        </section>
       </div>
     </main>
   );
---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,57 @@
+interface Movie {
+  original_title: string;
+  vote_average: number;
+  poster_path: string;
+  release_date: string;
+  original_language: string;
+  // add other properties as needed
+}
+
+interface Props {
+  key: number;
+  movie: Movie;
+}
+
+const MovieCard = ({
+  key,
+  movie: {
+    original_title,
+    poster_path,
+    vote_average,
+    original_language,
+    release_date,
+  },
+}: Props) => {
+  return (
+    <div className="movie-card">
+      <li key={key} className="text-white">
+        <img
+          src={
+            poster_path
+              ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+              : "/no-movie.png"
+          }
+          alt={original_title}
+        />
+        <div className="mt-4">
+          <h3>{original_title}</h3>
+
+          <div className="content">
+            <div className="rating">
+              <img src="star.svg" alt="Star Icon" />
+              <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
+            </div>
+            <span>.</span>
+            <p className="lang">{original_language}</p>
+            <span>.</span>
+            <p className="year">
+              {release_date ? release_date.split("-")[0] : "N/A"}
+            </p>
+          </div>
+        </div>
+      </li>
+    </div>
+  );
+};
+
+export default MovieCard;
